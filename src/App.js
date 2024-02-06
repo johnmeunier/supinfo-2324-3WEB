359570d0014e17927e343e32838756cf77d149e9
Johnathan MEUNIER <johnathan2693@gmail.com>
2024-02-06 10:02:26 +0100

    feat(Routeur): mise en place du routeur

--- src/App.js
+++ src/App.js
@@ -1,61 +1,31 @@
-import { useState, useEffect } from "react";
-import Limit from "./components/Limit";
-import Pagination from "./components/Pagination";
-import { modifySearchParam } from "./url.pure";
 import "./App.css";
+import HomePage from "./pages/Homepage";
+import PokemonList from "./pages/Pokemon/List";
+import PokemonDetails from "./pages/Pokemon/Details";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+  },
+  {
+    path: "pokemon/",
+    element: <PokemonList />,
+  },
+  {
+    path: "pokemon/:name",
+    element: <PokemonDetails />,
+  },
+]);
 
 const App = () => {
-  const [pokemons, setPokemons] = useState([]);
-  const [previousUrl, setPreviousUrl] = useState("");
-  const [nextUrl, setNextUrl] = useState("");
-  const [limit, setLimit] = useState(10);
-  const [count, setCount] = useState(null);
-  const [urlToFetch, setUrlToFetch] = useState(
-    `https://pokeapi.co/api/v2/pokemon?offset=0&limit=${limit}`
-  );
-
-  useEffect(() => {
-    async function fetchData() {
-      const promiseGetAllPokemons = await fetch(urlToFetch);
-      const getAllPokemonsJSON = await promiseGetAllPokemons.json();
-      setPreviousUrl(getAllPokemonsJSON.previous);
-      setNextUrl(getAllPokemonsJSON.next);
-      setCount(getAllPokemonsJSON.count);
-      setPokemons(getAllPokemonsJSON.results);
-    }
-    fetchData();
-  }, [urlToFetch]);
-
-  useEffect(() => {
-    setUrlToFetch((url) => modifySearchParam(url, "limit", limit));
-  }, [limit]);
-
-  return (
-    <>
-      <h2>Pokemon</h2>
-      <Limit setLimit={setLimit} />
-      <h3>Compteur : {pokemons.length}</h3>
-      {pokemons.length > 0 ? (
-        <>
-          <ol>
-            {pokemons.map((pokemon) => (
-              <li key={pokemon.name}>{pokemon.name}</li>
-            ))}
-          </ol>
-          <Pagination
-            previousUrl={previousUrl}
-            nextUrl={nextUrl}
-            setUrlToFetch={setUrlToFetch}
-            urlToFetch={urlToFetch}
-            limit={limit}
-            count={count}
-          />
-        </>
-      ) : (
-        "Chargement en cours"
-      )}
-    </>
-  );
+  return <RouterProvider router={router} />;
 };
 
 export default App;
---
+++ src/pages/Pokemon/Details.jsx
@@ -0,0 +1,5 @@
+export default () => {
+  return (
+    <h2>Details</h2>
+  )
+}
