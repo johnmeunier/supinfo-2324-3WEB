f4284a7a045e8754555e735dc15b2f789dc9f4ba
Johnathan MEUNIER <johnathan2693@gmail.com>
2024-02-06 08:56:49 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import Limit from "./components/Limit";
+import Pagination from "./components/Pagination";
+import { modifySearchParam } from "./url.pure";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const App = () => {
+  const [pokemons, setPokemons] = useState([]);
+  const [previousUrl, setPreviousUrl] = useState("");
+  const [nextUrl, setNextUrl] = useState("");
+  const [limit, setLimit] = useState(10);
+  const [count, setCount] = useState(null);
+  const [urlToFetch, setUrlToFetch] = useState(
+    `https://pokeapi.co/api/v2/pokemon?offset=0&limit=${limit}`
   );
-}
+
+  useEffect(() => {
+    async function fetchData() {
+      const promiseGetAllPokemons = await fetch(urlToFetch);
+      const getAllPokemonsJSON = await promiseGetAllPokemons.json();
+      setPreviousUrl(getAllPokemonsJSON.previous);
+      setNextUrl(getAllPokemonsJSON.next);
+      setCount(getAllPokemonsJSON.count);
+      setPokemons(getAllPokemonsJSON.results);
+    }
+    fetchData();
+  }, [urlToFetch]);
+
+  useEffect(() => {
+    setUrlToFetch((url) => modifySearchParam(url, "limit", limit));
+  }, [limit]);
+
+  return (
+    <>
+      <h2>Pokemon</h2>
+      <Limit setLimit={setLimit} />
+      <h3>Compteur : {pokemons.length}</h3>
+      {pokemons.length > 0 ? (
+        <>
+          <ol>
+            {pokemons.map((pokemon) => (
+              <li key={pokemon.name}>{pokemon.name}</li>
+            ))}
+          </ol>
+          <Pagination
+            previousUrl={previousUrl}
+            nextUrl={nextUrl}
+            setUrlToFetch={setUrlToFetch}
+            urlToFetch={urlToFetch}
+            limit={limit}
+            count={count}
+          />
+        </>
+      ) : (
+        "Chargement en cours"
+      )}
+    </>
+  );
+};
 
 export default App;
